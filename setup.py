b61adbeb3996d6755b02f48d663c20d198ec8239
2press <pres.sure@ymail.com>
2018-07-30 16:04:30 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='sc2monitor',
+      version='0.1',
+      description='When executed regularly keeps track of large amount StarCraft 2 accounts on the 1vs1 ladder',
+      url='https://github.com/2press/sc2monitor',
+      author='pressure',
+      author_email='pres.sure@ymail.com',
+      license='MIT',
+      packages=['sc2monitor'],
+      install_requires=[
+        #   'mysqlclient',
+          'requests'
+      ],
+      zip_safe=False)
