d149b1aca8af04df222a5daa3f43b86190ef6f1e
2press <pres.sure@ymail.com>
2018-08-01 16:11:14 +0200

    Included README.md in setup

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup
 
+# read the contents of README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='sc2monitor',
       version='0.1',
       description=('When executed regularly keeps track of large'
                    ' amount StarCraft 2 accounts on the 1vs1 ladder'),
+      long_description=long_description,
+      long_description_content_type='text/markdown'
       url='https://github.com/2press/sc2monitor',
       author='pressure',
       author_email='pres.sure@ymail.com',
