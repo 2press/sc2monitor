84d1c986e0308279654c3f771c6c32352b236291
2press <pres.sure@ymail.com>
2019-01-10 12:23:27 +0100

    Update conftest.py

--- test/conftest.py
+++ test/conftest.py
@@ -6,6 +6,12 @@ def pytest_addoption(parser):
                      default='', help="bnet apikey")
     parser.addoption("--apisecret", action="store",
                      default='', help="bnet secret")
+    parser.addoption("--db", action="store",
+                     default='localhost', help="database address")
+    parser.addoption("--user", action="store",
+                     default='sc2monitor', help="database user")
+    parser.addoption("--passwd", action="store",
+                     default='', help="database password")
 
 
 @pytest.fixture
@@ -16,3 +22,18 @@ def apikey(request):
 @pytest.fixture
 def apisecret(request):
     return request.config.getoption("--apisecret")
+
+
+@pytest.fixture
+def db(request):
+    return request.config.getoption("--db")
+
+
+@pytest.fixture
+def user(request):
+    return request.config.getoption("--user")
+
+
+@pytest.fixture
+def passwd(request):
+    return request.config.getoption("--passwd")
