767bec86af38ba5154dde844f2a0689f5e08d414
2press <pres.sure@ymail.com>
2019-01-10 13:57:58 +0100

    Update test_monitor.py

--- test/test_monitor.py
+++ test/test_monitor.py
@@ -1,22 +1,31 @@
 import asyncio
 
 from sc2monitor.controller import Controller
-from sc2monitor.model import Log, Player, Match
+from sc2monitor.model import Log, Player, Match, Server
 
 
 async def monitor_loop(**kwargs):
     async with Controller(**kwargs) as ctrl:
+        
         assert await ctrl.sc2api.get_access_token() != ''
+        
         ctrl.add_player('https://starcraft2.com/en-gb/profile/2/1/221986')
+       
         await ctrl.run()
         assert ctrl.sc2api.request_count > 0
+
         errors = ctrl.db_session.query(Log).filter(
             Log.level == 'ERROR').count()
         assert errors == 0
+        
         player = ctrl.db_session.query(Player).filter(
             Player.player_id == 221986).limit(1).scalar()
         assert player is not None
         assert player.name != ''
+        assert player.realm == 1
+        assert player.server == Server.Europe
+        assert player.player_id == 221986
+        
         matches = ctrl.db_session.query(Match).filter(Match.player == player).count()
         assert matches <= 25
 
