c81c7ddb0d11d3d445a76087b86b5a18e9ff8048
2press <pres.sure@ymail.com>
2019-01-13 11:59:16 +0100

    Whitespaces removed

--- sc2monitor/controller.py
+++ sc2monitor/controller.py
@@ -128,19 +128,19 @@ class Controller:
             self.create_db_session()
             close_db = True
         server, realm, player_id = self.sc2api.parse_profile_url(url)
-        
+
         for player in self.db_session.query(model.Player).filter(
                 model.Player.realm == realm,
                 model.Player.player_id == player_id,
                 model.Player.server == server).all():
             self.db_session.delete(player)
-            
+
         self.db_session.commit()
-        
+
         if close_db:
             self.db_session.close()
             self.db_session = None
-            
+
     async def update_season(self, server: model.Server):
         current_season = await self.sc2api.get_season(server)
         season = self.db_session.query(model.Season).\
