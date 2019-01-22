7c013870b0080deecf51eb746f7eeab05c70b6ce
2press <pres.sure@ymail.com>
2019-01-22 14:42:01 +0100

    Update handlers.py

--- sc2monitor/handlers.py
+++ sc2monitor/handlers.py
@@ -12,16 +12,25 @@ class SQLAlchemyHandler(logging.Handler):
         """Init logger and set database session."""
         super().__init__()
         self.db_session = db_session
+        self.errors = 0
+        self.warnings = 0
 
     def emit(self, record):
         """Write a record to the database."""
         trace = None
         exc = record.__dict__['exc_info']
+        level = record.__dict__['levelname']
+
+        if level in ['ERROR', 'CRITICAL']:
+            self.errors += 1
+        elif level == 'WARNING':
+            self.warnings += 1
+
         if exc:
             trace = traceback.format_exc()
         log = Log(
             logger=record.__dict__['name'],
-            level=record.__dict__['levelname'],
+            level=level,
             trace=trace,
             msg=record.__dict__['msg'],)
         self.db_session.add(log)
