4b1d2b82eb4c883b10a814ea91c8cdbf0bca5f43
2press <pres.sure@ymail.com>
2020-06-26 19:58:32 +0200

    [requires.io] dependency update (#18)
    Co-authored-by: requires.io <support@requires.io>

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(name='sc2monitor',
       install_requires=[
           'PyMySQL >= 0.9.3',
           'aiohttp >= 3.6.2',
-          'sqlalchemy==1.3.17'
+          'sqlalchemy==1.3.18'
       ],
       zip_safe=False,
       classifiers=[
